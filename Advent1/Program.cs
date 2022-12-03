21ebad5185175730ae188db35d5601328a0159e2
Niels Schuler <skizschu@gmail.com>
2022-12-03 20:03:51 +0100

    finished

--- Advent1/Program.cs
+++ Advent1/Program.cs
@@ -1,1 +1,34 @@
-﻿string inutFileName = "calories.txt";
+﻿using Advent1.model;
+
+string inutFileName = "calories.txt";
+
+string fileData = File.ReadAllText(inutFileName);
+
+List<Elf> allElfs = new List<Elf>();
+
+string[] perElf = fileData.Split(Environment.NewLine + Environment.NewLine);
+
+int elfCount = 1;
+
+foreach(string allCaloriePerElf in perElf)
+{
+    List<int> calories = new List<int>();
+    string[] notProcessedCalories = allCaloriePerElf.Split(Environment.NewLine);
+
+    foreach (string singleCalorie in notProcessedCalories)
+    {
+        calories.Add(int.Parse(singleCalorie));
+    }
+
+    allElfs.Add(new Elf
+    {
+        number = elfCount,
+        calories = calories,
+        SumCalories = calories.Sum()
+    });
+    elfCount++;
+}
+
+Elf max = allElfs.MaxBy(elf => elf.SumCalories);
+
+Console.WriteLine("a");
---
+++ Advent1/model/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent1.model
+{
+    internal class Elf
+    {
+        internal int number;
+        internal List<int> calories;
+
+        internal int SumCalories;
+    }
+}
