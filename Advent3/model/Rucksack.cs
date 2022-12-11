0b6759aecf1fcd30002c15f55f35591f14f56853
Niels Schuler <skizschu@gmail.com>
2022-12-11 10:27:45 +0100

    New Method

--- Advent3/model/Rucksack.cs
+++ Advent3/model/Rucksack.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,78 @@ namespace Advent3.model
 {
     internal class Rucksack
     {
+        internal string FirstPart { get; set; }
+        internal string SecondPart { get; set; }
+
+        internal List<string> GetDuplicates()
+        {
+            List<string> Result = new List<string>();
+
+            foreach (char item in FirstPart)
+            {
+                if(SecondPart.Contains(item))
+                {
+                    Result.Add(item.ToString());
+                }
+            }
+            Result = Result.Distinct().ToList();
+            return Result;
+        }
+
+        internal enum ItemCost
+        {
+            a = 1,
+            b = 2,
+            c = 3,
+            d = 4,
+            e = 5,
+            f = 6,
+            g = 7,
+            h = 8,
+            i = 9,
+            j = 10,
+            k = 11,
+            l = 12,
+            m = 13,
+            n = 14,
+            o = 15,
+            p = 16,
+            q = 17,
+            r = 18,
+            s = 19,
+            t = 20,
+            u = 21,
+            v = 22,
+            w = 23,
+            x = 24,
+            y = 25,
+            z = 26,
+            A = 27,
+            B = 28,
+            C = 29,
+            D = 30,
+            E = 31,
+            F = 32,
+            G = 33,
+            H = 34,
+            I = 35,
+            J = 36,
+            K = 37,
+            L = 38,
+            M = 39,
+            N = 40,
+            O = 41,
+            P = 42,
+            Q = 43,
+            R = 44,
+            S = 45,
+            T = 46,
+            U = 47,
+            V = 48,
+            W = 49,
+            X = 50,
+            Y = 51,
+            Z = 52
+        }
     }
 }
