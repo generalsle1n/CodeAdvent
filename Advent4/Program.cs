a991e15b5e96e2705d0eaa9ed89ad8e6c81368d8
Niels Schuler <skizschu@gmail.com>
2022-12-12 19:10:17 +0100

    Working

--- Advent4/Program.cs
+++ Advent4/Program.cs
@@ -1,1 +1,44 @@
 ﻿//https://adventofcode.com/2022/day/4
+using Advent4.model;
+
+string input = "input.txt";
+string[] allLines = File.ReadAllLines(input);
+
+List<ElfPair> elfPairs= new List<ElfPair>();
+
+foreach(string line in allLines)
+{
+    List<string> RawSections = line.Split(",").ToList();
+    List<int> SectionIndex = new List<int>();
+    
+    foreach(string section in RawSections)
+    {
+        string[] RawIndex = section.Split("-");
+        foreach(string index in RawIndex)
+        {
+            SectionIndex.Add(int.Parse(index));
+        }
+    }
+
+    ElfPair cur = new ElfPair()
+    {
+        First = new Section
+        {
+            Start = SectionIndex[0],
+            End = SectionIndex[1]
+        },
+        Second = new Section
+        {
+            Start = SectionIndex[2],
+            End = SectionIndex[3]
+        }
+    };
+
+    cur.Overlaped = cur.CheckIfOverlaped();
+
+    elfPairs.Add(cur);
+}
+
+List<ElfPair> allOverlapped = elfPairs.FindAll(Pair => Pair.Overlaped == true);
+
+Console.WriteLine(allOverlapped.Count);
---
+++ Advent4/model/ElfPair.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent4.model
+{
+    internal class ElfPair
+    {
+        internal Section First { get; set; }
+        internal Section Second { get; set; }
+
+        internal bool Overlaped { get; set; }
+
+        internal bool CheckIfOverlaped()
+        {
+            HashSet<int> first = CreateArray(First.Start, First.End).ToHashSet();
+            HashSet<int> second = CreateArray(Second.Start, Second.End).ToHashSet();
+
+            if (first.IsSubsetOf(second) || second.IsSubsetOf(first))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private int[] CreateArray(int Start, int End)
+        {
+            List<int> array = new List<int>();
+            int startCount = Start;
+
+            while(startCount <= End)
+            {
+                array.Add(startCount);
+                startCount++;
+            }
+
+            return array.ToArray();
+        }
+    }
+
+    internal class Section
+    {
+        internal int Start { get; set; }
+        internal int End { get; set; }
+    }
+}
