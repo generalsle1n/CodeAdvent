2ca657fd70737e57ec58166a3fea6f81de06431a
generalsle1n <niels.schuler@web.de>
2023-12-23 20:09:05 +0100

    Solved Advent1

---
+++ 2023/2023 - Advent1/Program.cs
@@ -0,0 +1,25 @@
+﻿//https://adventofcode.com/2023/day/1
+
+string FileName = "PuzzleInput.txt";
+string[] Content = File.ReadAllLines(FileName);
+
+int Count = 0;
+
+foreach(string Item in Content)
+{
+	string Number = null;
+	char[] SingleContent = Item.ToArray();
+	foreach(char Character in SingleContent)
+	{
+		if (Char.IsDigit(Character))
+		{
+			Number += Character;
+		}
+	}
+
+	Number = Number[0] + Number[Number.Length -1].ToString();
+
+	Count += int.Parse(Number);
+}
+
+Console.WriteLine(Count);
