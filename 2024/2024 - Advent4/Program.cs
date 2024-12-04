29f9c6b09d01d71e37c0c1f8ef5a6d3e94b38a55
Niels Schuler <niels.schuler@web.de>
2024-12-04 19:35:15 +0100

    Just Part One

---
+++ 2024/2024 - Advent4/Program.cs
@@ -0,0 +1,55 @@
+﻿using _2024___Advent4;
+using System;
+
+string InputFileName = "Input.txt";
+string[] InputFileData = File.ReadAllLines(InputFileName);
+
+List<List<string>> MatrixData = new List<List<string>>();
+
+foreach (string SingleLine in InputFileData)
+{
+    List<string> Data = SingleLine.ToList().Select(item => item.ToString()).ToList();
+    MatrixData.Add(Data);
+}
+
+
+int ResultPartOne = 0;
+
+//Horizontal
+foreach (List<string> SingleLine in MatrixData)
+{
+    string Data = StringChecker.GetStringFromHorizontal(SingleLine);
+    ResultPartOne += StringChecker.CountInString(Data);
+}
+
+//Vertical
+int ListLength = MatrixData[0].Count;
+int Count = 0;
+
+while(Count < ListLength)
+{
+    string Data = StringChecker.GetStringFromVertical(MatrixData, Count);
+    ResultPartOne += StringChecker.CountInString(Data);
+    Count++;
+}
+
+//Diagonal
+List<string> LeftToRight = StringChecker.GetStringFromDiagonalLeftToRight(MatrixData);
+
+foreach(string SingleLine in LeftToRight)
+{
+    ResultPartOne += StringChecker.CountInString(SingleLine);
+}
+
+List<string> RightToLeft = StringChecker.GetStringFromDiagonalRightToLeft(MatrixData);
+
+foreach (string SingleLine in RightToLeft)
+{
+    ResultPartOne += StringChecker.CountInString(SingleLine);
+}
+
+Console.WriteLine($"Result Part One: {ResultPartOne}");
+
+string InputFileDataPartTwo = File.ReadAllText(InputFileName);
+
+StringChecker.FindXInString(InputFileData);
---
+++ 2024/2024 - Advent4/StringChecker.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _2024___Advent4
+{
+    internal class StringChecker
+    {
+        private static Regex _regexCorrect = new Regex("XMAS");
+        private static Regex _regexReverse = new Regex("SAMX");
+
+        internal static int CountInString(string Input)
+        {
+            int Count = 0; 
+            Count += _regexCorrect.Count(Input);
+            Count += _regexReverse.Count(Input);
+
+            return Count;
+        }
+
+        internal static string GetStringFromHorizontal(List<string> Input)
+        {
+            StringBuilder Data = new StringBuilder();
+
+            foreach (string InputItem in Input)
+            {
+                Data.Append(InputItem);
+            }
+
+            return Data.ToString();
+        }
+
+        internal static string GetStringFromVertical(List<List<string>> Input, int Index)
+        {
+            StringBuilder Data = new StringBuilder();
+            
+            foreach(List<string> SingleLine in Input)
+            {
+                Data.Append(SingleLine[Index]);
+            }
+
+            return Data.ToString();
+        }
+
+        internal static List<string> GetStringFromDiagonalLeftToRight(List<List<string>> Input)
+        {
+            List<string> Result = new List<string>();
+            string[][] RawData = Input.SelectMany((row, rowIdx) => row.Select((x, colIdx) => new { Key = rowIdx - colIdx, Value = x })).GroupBy(x => x.Key, (key, values) => values.Select(i => i.Value).ToArray()).ToArray();
+
+            foreach(string[] InputItem in RawData)
+            {
+                StringBuilder Builder = new StringBuilder();
+
+                foreach(string SingleData in InputItem)
+                {
+                    Builder.Append(SingleData);
+                }
+                Result.Add(Builder.ToString());
+            }
+
+            return Result;
+        }
+
+        internal static List<string> GetStringFromDiagonalRightToLeft(List<List<string>> Input)
+        {
+            List<string> Result = new List<string>();
+            string[][] RawData = Input.SelectMany((row, rowIdx) => row.Select((x, colIdx) => new { Key = rowIdx + colIdx, Value = x })).GroupBy(x => x.Key, (key, values) => values.Select(i => i.Value).ToArray()).ToArray();
+
+            foreach (string[] InputItem in RawData)
+            {
+                StringBuilder Builder = new StringBuilder();
+
+                foreach (string SingleData in InputItem)
+                {
+                    Builder.Append(SingleData);
+                }
+                Result.Add(Builder.ToString());
+            }
+
+            return Result;
+        }
+        
+        internal static int FindXInString(string[] Input)
+        {
+            int Result = 0;
+
+            int MaxCount = Input.Length - 1;
+            int Count = 0;
+
+            while(Count <= MaxCount)
+            {
+                int MaxCharCount = Input[Count].Length - 1;
+                int CharCount = 0;
+
+            }
+
+            return Result;
+        }
+    }
+}
